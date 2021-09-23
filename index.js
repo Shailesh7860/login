3f0c9e35bc6b4a50c945df39f024c7f774242755
Shailesh Suvarna <shaileshsuvarna24@gmail.com>
2021-09-23 19:53:58 +0530

    Login Trail

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const SignIn = () => {
       var token = credential.accessToken;
       var user = result.user;
       console.log(user);
-      window.location.href = "Aboutme.html";
+      window.location.href = "https://shaileshsuvarna.github.io/Aboutme/Aboutme.html";
     })
     .catch((error) => {
       console.log(error);
