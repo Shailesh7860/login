0a9183e76b7f4acdd19907dc8b244234d13cc0bb
Shailesh7860 <shaileshsuvarna24@gmail.com>
2022-05-10 12:49:31 +0530

    newly added email login

--- login.js
+++ login.js
@@ -30,3 +30,25 @@ const SignIn = () => {
     });
 };
 
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
